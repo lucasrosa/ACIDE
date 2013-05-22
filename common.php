8eaf2cb71ad696069c118d7dc9df6a923ed9afea
Lucas Furlani <lrosa@upei.ca>
2013-05-22 14:32:37 -0300

    Testing merging

--- common.php
+++ common.php
@@ -3,12 +3,13 @@
     *  Copyright (c) Codiad & Kent Safranski (codiad.com), distributed
     *  as-is and without warranty under the MIT License. See
     *  [root]/license.txt for more. This information must remain intact.
-    */
+    */ 
+	
      
     Common::startSession();
                
     //////////////////////////////////////////////////////////////////
-    // Common Class
+    // Common Class //
     //////////////////////////////////////////////////////////////////
     
     class Common {
