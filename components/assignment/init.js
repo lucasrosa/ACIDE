8e749be96c1b92e85962b910c063bc2795252ac6
lucasfurlani <lrosa@upei.ca>
2013-06-11 17:55:43 -0300

    Minor bug fixes. Inserted Assignments button in the Menu

--- components/assignment/init.js
+++ components/assignment/init.js
@@ -1,1 +1,3 @@
-  
+$(document).ready(function() {
+	$('.sb-right-content').prepend("<a href='http://hci.csit.upei.ca/Codiad/components/assignment/' target='_blank'><span class=\"icon-docs l bigger-icon\"></span>Assignments</a>");
+});
