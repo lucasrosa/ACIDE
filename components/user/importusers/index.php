e4f6040019bfed1b0d7794d18c1a4f6b4d1df6d1
lucasfurlani <lrosa@upei.ca>
2013-06-18 14:56:13 -0300

    #32 implemented. Return button added in the User Import page

--- components/user/importusers/index.php
+++ components/user/importusers/index.php
@@ -74,6 +74,22 @@
 			}
 		} 
 	}
+
+	$pageURL = 'http';
+	
+	if (@$_SERVER["HTTPS"] == "on") {
+		$pageURL .= "s";
+	}
+		$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+		 $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
+	} else {
+		$pageURL .= $_SERVER["SERVER_NAME"];
+	}
+	
+	$dir =  dirname($_SERVER['PHP_SELF']);
+	$dirs = explode('/', $dir);
+	$pageURL .= "/" . $dirs[1];
 ?>
 <!doctype html>
 
@@ -87,6 +103,9 @@
   	
 </head>
 <body>
+	<form method="post" action="<?=$pageURL?>">
+		<button>Open IDE</button>
+	</form>
 	<h1  align="center">Import Users</h1>
 	
 	
