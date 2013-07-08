7df32dc8dad3cd1cfc872c879b58f2364c3b7410
lucasfurlani <lrosa@upei.ca>
2013-07-08 15:32:56 -0300

    #13 implemented the file log code. The system knows if any file has focus and the path of the active file with focus.

--- components/userlog/log_script.js
+++ components/userlog/log_script.js
@@ -1,5 +1,7 @@
-var hasFocus = true;
-var checkFocusInterval = 5; // seconds
+var hasFocus 					= true;
+var projectHasFocus 			= true;
+var checkFocusInterval 			= 5; // seconds
+var checkFocusForFileInterval 	= 0.5; // seconds
 
 (function(global, $){
 	var codiad = global.codiad;
@@ -7,6 +9,7 @@ var checkFocusInterval = 5; // seconds
 	codiad.userlog.logUserHasFocusOnTheSystem();
 	
 	setInterval ("CheckFocus()", (checkFocusInterval * 1000));
+	setInterval ("CheckFocusForFile()", (checkFocusForFileInterval * 1000));
 	
 	window.onfocus = function() {
     	hasFocus = true;
@@ -16,17 +19,35 @@ var checkFocusInterval = 5; // seconds
 		hasFocus = false;
 	};
 	
+	// Project logging
+	
+	// File logging
+	
+	$(document).ready(function() {
+		$('.ace_line').attr('id', 'file_input');
+	});
+	
+	
 })(this, jQuery);
 
 function CheckFocus () {
 	
 	if (hasFocus) {
-		//console.log("The document has the focus.");
+		//console.log("The document has focus.");
 		codiad.userlog.logUserHasFocusOnTheSystem();
-	} 
+	}
 	/*
 	else {
 		console.log("The document doesn't have the focus.");
     }
     */
+}
+
+function CheckFocusForFile () {
+	var file = document.getElementById('file_input');
+	var filename = codiad.active.getPath();
+	if ((file = document.activeElement) && file != document.body && document.activeElement.tagName == 'TEXTAREA')
+	        console.log("The focus is on the file '"+filename+"'.");
+	    else
+	        console.log("There is not focus on any file.");
 }
