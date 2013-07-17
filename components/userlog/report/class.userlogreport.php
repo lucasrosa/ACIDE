d1180d7b3d89ba53f42f26575632832052126489
lucasfurlani <lrosa@upei.ca>
2013-07-17 11:41:21 -0300

    #79 Enhanced GetTimeSpentInProjectsInSession  method and fixed bugs

--- components/userlog/report/class.userlogreport.php
+++ components/userlog/report/class.userlogreport.php
@@ -89,52 +89,37 @@ class Userlogreport {
 
 	public function GetTimeSpentInProjectsInSession ($session_id, $return_projects_without_logs = TRUE) {
 		$this->userlog = new Userlog();
-		
 		$this->userlog->username = $this->username;
-		$sessions = $this->userlog->GetAllSessionsForUser();
-		$total_time_system = new DateTime('0000-00-00 00:00:00');
-		$total_time_system_helper = clone $total_time_system;
 		$projects_time = array();
 		
-		foreach($sessions as $session) {
-			$date1 = new DateTime($session['start_timestamp']);
-			$date2 = new DateTime($session['last_update_timestamp']);
-			
-			$interval = $date1->diff($date2);
-			//$total_time_system->add($interval);
-			
-			$Project = new Project();
-			$projects = $Project->GetProjectsForUser($this->username);
-			
-			
-			foreach($projects as $project) {
-				error_log("here!!! eh? session id = $session_id");
-				$this->userlog->path = $project['path'];
-				$project_sessions = $this->userlog->GetAllLogsForProject($session_id);
+		$Project = new Project();
+		$projects = $Project->GetProjectsForUser($this->username);
 				
-				if ($project_sessions->count() > 0 || $return_projects_without_logs) {
-					error_log("here eh?");
+		foreach($projects as $project) {
+			$this->userlog->path = $project['path'];
+			$project_sessions = $this->userlog->GetAllLogsForProject($session_id);
+			
+			if ($project_sessions->count() > 0 || $return_projects_without_logs) {
+				$total_time_project = new DateTime('0000-00-00 00:00:00');
+				$total_time_project_helper = clone $total_time_project;
+			
+				foreach ($project_sessions as $project_session) {
+					$date1 = new DateTime($project_session['start_timestamp']);
+					$date2 = new DateTime($project_session['last_update_timestamp']);
+					$interval = $date1->diff($date2);
 					
-					$total_time_project = new DateTime('0000-00-00 00:00:00');
-					$total_time_project_helper = clone $total_time_project;
-				
-					foreach ($project_sessions as $project_session) {
-						$date1 = new DateTime($project_session['start_timestamp']);
-						$date2 = new DateTime($project_session['last_update_timestamp']);
-						$interval = $date1->diff($date2);
-						
-						$total_time_project->add($interval);
-					}
-				
-					$total_time_project_interval = $total_time_project_helper->diff($total_time_project);
-					
-					$current_project_time = array();
-					$current_project_time['path'] = $project['path'];
-					$current_project_time['interval'] = $total_time_project_interval;
-					$projects_time[] = $current_project_time;
+					$total_time_project->add($interval);
 				}
+			
+				$total_time_project_interval = $total_time_project_helper->diff($total_time_project);
+				
+				$current_project_time = array();
+				$current_project_time['path'] = $project['path'];
+				$current_project_time['interval'] = $total_time_project_interval;
+				$projects_time[] = $current_project_time;
 			}
 		}
+		
 		return $projects_time;
 	}
 	
