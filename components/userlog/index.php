aedca67b93392759d952c2667b5ae3665ebbb93f
lucasfurlani <lrosa@upei.ca>
2013-08-12 17:22:00 -0300

    #79 updated page that shows all the links to the log report's charts.

--- components/userlog/index.php
+++ components/userlog/index.php
@@ -51,6 +51,36 @@
 		$User = new User();
 		$User->username = $_SESSION['user'];
 		$user_courses = $User->GetUserCourses();
+			
+			
+		if (isset($_POST['action']) && $_POST['action'] ==  'generate_chart') {
+			$report = $_POST['report'];
+			$direct_page = $pageURL . "/components/userlog/report";
+			switch ($report) {
+				case 'compilation-errors':
+						header("Location: ". $direct_page . "/compilation_errors/compilation_errors.php");
+					break;
+				case 'compilation-errors-assignment':
+						header("Location: ". $direct_page . "/compilation_errors_assignment/compilation_errors_assignment.php");
+					break;
+				case 'compilation-errors-ratio':
+						header("Location: ". $direct_page . "/compilation_errors_ratio/compilation_errors_ratio.php");
+					break;
+				case 'sessions': 
+						header("Location: ". $direct_page . "/session/log_session_report.php");
+					break;
+				case 'time-project':
+						header("Location: ". $direct_page . "/time_project/time_project_report.php");
+					break;
+				case 'time-system':
+						header("Location: ". $direct_page . "/time_system/time_system_report.php");
+					break;
+				  
+				default:
+						echo "nah!";
+					break;
+			}
+		}
 		
 	?>
 	<!doctype html>
@@ -64,9 +94,6 @@
 		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 	  	<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 	  	<script src="/Codiad/components/assignment/init.js"></script>
-	  	<!-- Timepicker -->
-	  	<link rel="stylesheet" type="text/css" href="timepicker/jquery.ptTimeSelect.css" />
-	    <script type="text/javascript" src="timepicker/jquery.ptTimeSelect.js" /></script>   
 	  	
 	</head>
 	<body>
@@ -78,43 +105,48 @@
 		<div id="modal" style="display: block; width: 1000px; margin:0 auto;" >
 			<div id="modal-content">
 				<!-- <label>Assignment List</label> -->
-				<div id="project-list">
-					<table width="100%">
-						<tbody>
-							<tr>
-								<th>Course</th>
-								<th>Report</th>
-							</tr>
-							<tr>
-								<td>
-									<select>
-										<?
-											$Course = new Course();
-											for ($i = 0; $i < count($user_courses); $i++) {
-												$Course->id = $user_courses[$i];
-												$Course->Load();
-										?>
-									  		<option value="<?=$Course->id?>"><?=$Course->name?></option>
-									  	<?
-											}
-									  	?>
-									</select>
-								</td>
-								<td>
-									<select>
-									  <option value="volvo">Volvo</option>
-									  <option value="saab">Saab</option>
-									  <option value="mercedes">Mercedes</option>
-									  <option value="audi">Audi</option>
-									</select>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<div align="center">
-						<button>Generate Chart</button>
+				<form method="post">
+					<input type="hidden" name="action" value="generate_chart" />
+					<div id="project-list">
+						<table width="100%">
+							<tbody>
+								<tr>
+									<th>Course</th>
+									<th>Report</th>
+								</tr>
+								<tr>
+									<td>
+										<select name="course">
+											<?
+												$Course = new Course();
+												for ($i = 0; $i < count($user_courses); $i++) {
+													$Course->id = $user_courses[$i];
+													$Course->Load();
+											?>
+										  		<option value="<?=$Course->id?>"><?=$Course->name?></option>
+										  	<?
+												}
+										  	?>
+										</select>
+									</td>
+									<td>
+										<select name="report">
+										  <option value="compilation-errors">Compilation Errors</option>
+										  <option value="compilation-errors-assignment">Compilation Errors with Assignments</option>
+										  <option value="compilation-errors-ratio">Compilation Errors Ratio</option>
+										  <option value="sessions">Session</option>
+										  <option value="time-project">Time spent in Project</option>
+										  <option value="time-system">Time spent in System</option>
+										</select>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<div align="center">
+							<button>Generate Chart</button>
+						</div>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	</body>
