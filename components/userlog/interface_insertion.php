09d958fdaa9c641c791eb862eff033768ca96d60
lucasfurlani <lrosa@upei.ca>
2013-08-13 13:48:26 -0300

    #90 added interface insertion to reports

---
+++ components/userlog/interface_insertion.php
@@ -0,0 +1,32 @@
+<?php
+	if (isset($_SESSION['user'])) {
+		// Instantiate the permission object and set the username
+		$Permission = new Permission($_SESSION['user']);
+		
+		$pageURL = 'http';
+		
+		if (@$_SERVER["HTTPS"] == "on") {
+			$pageURL .= "s";
+		}
+		
+		$pageURL .= "://";
+		if ($_SERVER["SERVER_PORT"] != "80") {
+			 $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
+		} else {
+			$pageURL .= $_SERVER["SERVER_NAME"];
+		}
+		
+		$dir =  dirname($_SERVER['PHP_SELF']);
+		$dirs = explode('/', $dir);
+		$pageURL .= "/" . $dirs[1] . "/components/userlog";
+		
+		if ($Permission->GetPermissionToSeeReports()) {
+			echo("<script>
+				$(document).ready(function() {
+					$('.sb-right-content').prepend('<a href=\'" .$pageURL . "\' target=\'_blank\' style=\'text-decoration: none\'><span class=\'icon-chart-bar l bigger-icon\'></span>Reports</a>');
+				});	
+			</script>");
+		}		
+	}
+?>
+
