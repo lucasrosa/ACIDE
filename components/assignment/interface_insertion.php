0472753db4e0e9eb51c5c444f5a0195289910f48
Lucas Furlani <lrosa@upei.ca>
2013-10-01 10:18:29 -0300

    Updated assignment/interface_insertion.php addressing issue #116

--- components/assignment/interface_insertion.php
+++ components/assignment/interface_insertion.php
@@ -4,21 +4,31 @@
 		$Permission = new Permission($_SESSION['user']);
 		
 		$pageURL = 'http';
-		
 		if (@$_SERVER["HTTPS"] == "on") {
 			$pageURL .= "s";
 		}
 		
 		$pageURL .= "://";
+		
 		if ($_SERVER["SERVER_PORT"] != "80") {
 			 $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
 		} else {
 			$pageURL .= $_SERVER["SERVER_NAME"];
 		}
 		
-		$dir =  dirname($_SERVER['PHP_SELF']);
-		$dirs = explode('/', $dir);
-		$pageURL .= "/" . $dirs[1] . "/components/assignment";
+		$url =  $pageURL;
+		$directory = explode("/", $_SERVER["REQUEST_URI"]);
+		    
+		for ($i = 1; $i < count($directory); $i++) {
+		    if ($directory[$i] == "components") {
+		        break;
+		    } else {
+		        $url .= "/" . $directory[$i];   
+		    }
+		}
+		
+		$pageURL = $url;
+		$pageURL .= "/components/assignment";
 		
 		if ($Permission->GetPermissionToSeeAssignments()) {
 			echo("<script>
