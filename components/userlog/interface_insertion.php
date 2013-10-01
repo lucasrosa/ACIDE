4f4de304816f9cd5cb30b230b9389df25c326b9c
Lucas Furlani <lrosa@upei.ca>
2013-10-01 10:55:38 -0300

    Updated userlog/interface_insertion.php addressing issue #116

--- components/userlog/interface_insertion.php
+++ components/userlog/interface_insertion.php
@@ -4,21 +4,30 @@
 		$Permission = new Permission($_SESSION['user']);
 		
 		$pageURL = 'http';
-		
 		if (@$_SERVER["HTTPS"] == "on") {
 			$pageURL .= "s";
 		}
-		
+
 		$pageURL .= "://";
+
 		if ($_SERVER["SERVER_PORT"] != "80") {
 			 $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"];
 		} else {
 			$pageURL .= $_SERVER["SERVER_NAME"];
 		}
+
+		$url =  $pageURL;
+		$directory = explode("/", $_SERVER["REQUEST_URI"]);
+
+		for ($i = 1; $i < count($directory); $i++) {
+		    if ($directory[$i] == "components") {
+		        break;
+		    } else {
+		        $url .= "/" . $directory[$i];   
+		    }
+		}
 		
-		$dir =  dirname($_SERVER['PHP_SELF']);
-		$dirs = explode('/', $dir);
-		$pageURL .= "/" . $dirs[1] . "/components/userlog";
+		$pageURL = $url . "/components/userlog";
 		
 		if ($Permission->GetPermissionToSeeReports()) {
 			echo("<script>
